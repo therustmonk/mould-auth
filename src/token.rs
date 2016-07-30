d0112e2ac0c82ec1b8ad32d086017cdd2648de91
Denis Kolodin <deniskolodin@gmail.com>
2016-07-30 16:31:19 +0300

    Move to mould 0.0.14

--- src/token.rs
+++ src/token.rs
@@ -11,17 +11,17 @@ pub trait TokenChecker<R: Role>: 'static {
 /// A handler which use `TokenChecker` to set role to session.
 /// The following actions available:
 /// * `do-auth` - try to authorize by token
-pub struct TokenHandler<TC, R>
+pub struct TokenRouter<TC, R>
     where TC: TokenChecker<R>, R: Role {
     checker: Arc<Mutex<TC>>,
     _role: PhantomData<R>,
 }
 
-impl<TC, R> TokenHandler<TC, R>
+impl<TC, R> TokenRouter<TC, R>
     where TC: TokenChecker<R>, R: Role {
 
     pub fn new(checker: TC) -> Self {
-        TokenHandler {
+        TokenRouter {
             checker: Arc::new(Mutex::new(checker)),
             _role: PhantomData,
         }
@@ -29,15 +29,11 @@ impl<TC, R> TokenHandler<TC, R>
 
 }
 
-impl<CTX, TC, R> Handler<CTX> for TokenHandler<TC, R>
+impl<CTX, TC, R> Router<CTX> for TokenRouter<TC, R>
     where CTX: Authorize<R>, TC: TokenChecker<R>, R: Role {
-    fn build(&self, mut request: Request) -> Box<Worker<CTX>> {
+    fn route(&self, _: &CTX, request: &Request) -> Box<Worker<CTX>> {
         if request.action == "do-auth" {
-            Box::new(TokenCheckWorker {
-                checker: self.checker.clone(),
-                token: request.extract("token"),
-                _role: PhantomData,
-            })
+            Box::new(TokenCheckWorker::new(self.checker.clone()))
         } else {
             let msg = format!("Unknown action '{}' for token service!", request.action);
             Box::new(RejectWorker::new(msg))
@@ -48,18 +44,24 @@ impl<CTX, TC, R> Handler<CTX> for TokenHandler<TC, R>
 struct TokenCheckWorker<TC, R>
     where TC: TokenChecker<R>, R: Role {
     checker: Arc<Mutex<TC>>,
-    token: Option<String>,
     _role: PhantomData<R>,
+}
+
+impl<TC, R> TokenCheckWorker<TC, R>
+    where TC: TokenChecker<R>, R: Role {
+    fn new(checker: Arc<Mutex<TC>>) -> Self {
+        TokenCheckWorker { checker: checker, _role: PhantomData }
+    }
 }
 
 impl<CTX, TC, R> Worker<CTX> for TokenCheckWorker<TC, R>
     where CTX: Authorize<R>, TC: TokenChecker<R>, R: Role {
-    fn shortcut(&mut self, session: &mut CTX) -> WorkerResult<Shortcut> {
-        let token = try!(self.token.take()
-            .ok_or(WorkerError::reject("No token provided!")));
+    fn prepare(&mut self, session: &mut CTX, mut request: Request) -> worker::Result<Shortcut> {
+        let token: String = try!(request.extract("token")
+            .ok_or(worker::Error::reject("No token provided!")));
         let role = {
             let mut guard = try!(self.checker.lock()
-                .or(Err(WorkerError::reject("Impossible to check token!"))));
+                .or(Err(worker::Error::reject("Impossible to check token!"))));
             guard.get_role_for_token(&token)
         };
         let success = role.is_some();
@@ -67,7 +69,7 @@ impl<CTX, TC, R> Worker<CTX> for TokenCheckWorker<TC, R>
         if success {
             Ok(Shortcut::Done)
         } else {
-            Err(WorkerError::reject("Token is not valid!"))
+            Err(worker::Error::reject("Token is not valid!"))
         }
     }
 }
