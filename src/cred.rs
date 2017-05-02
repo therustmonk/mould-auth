ff1b4becd925e169cbea35b36291eb2e0418fd4c
Denis Kolodin <deniskolodin@gmail.com>
2017-05-02 15:30:15 +0300

    Add logout action

--- src/cred.rs
+++ src/cred.rs
@@ -4,12 +4,14 @@ use super::Role;
 
 pub trait Manager<R: Role> {
     fn set_role(&mut self, login: &str, password: &str) -> Result<bool, &str>;
+    fn reset_role(&mut self) -> Result<(), &str>;
     fn attach_password(&mut self, password: &str) -> Result<(), &str>;
 }
 
 pub enum Permission {
-    CanAuth,
-    CanChange,
+    CanLogin,
+    CanLogout,
+    CanChangePassword,
 }
 
 impl Rights for Permission { }
@@ -38,6 +40,7 @@ impl<T, R> service::Service<T> for AuthService<R>
     fn route(&self, action: &str) -> service::Result<service::Action<T>> {
         match action {
             "do-login" => Ok(do_login::action()),
+            "do-logout" => Ok(do_logout::action()),
             "change-password" => Ok(change_password::action()),
             _ => Err(service::ErrorKind::ActionNotFound.into()),
         }
@@ -74,12 +77,43 @@ mod do_login {
         type Out = ();
 
         fn prepare(&mut self, session: &mut T, request: Self::Request) -> worker::Result<Shortcut> {
-            session.require(&Permission::CanAuth)?;
+            session.require(&Permission::CanLogin)?;
             if session.set_role(&request.login, &request.password)? {
                 Ok(Shortcut::Done)
             } else {
                 Ok(Shortcut::Reject("wrong credentials".into()))
             }
+        }
+    }
+}
+
+mod do_logout {
+    use super::*;
+
+    pub fn action<T, R>() -> service::Action<T>
+        where T: Session + Require<Permission> + Manager<R>, R: Role,
+    {
+        let worker = Worker {
+            _role: PhantomData,
+        };
+        service::Action::from_worker(worker)
+    }
+
+    struct Worker<R> {
+        _role: PhantomData<R>,
+    }
+
+    impl<T, R> worker::Worker<T> for Worker<R>
+        where T: Session + Require<Permission> + Manager<R>, R: Role,
+    {
+        type Request = ();
+        type In = ();
+        type Out = ();
+
+        fn prepare(&mut self, session: &mut T, _: Self::Request) -> worker::Result<Shortcut> {
+            session.require(&Permission::CanLogout)?;
+            session.reset_role()?;
+            Ok(Shortcut::Done)
         }
     }
 }
@@ -113,7 +147,7 @@ mod change_password {
         type Out = ();
 
         fn prepare(&mut self, session: &mut T, request: Self::Request) -> worker::Result<Shortcut> {
-            session.require(&Permission::CanChange)?;
+            session.require(&Permission::CanChangePassword)?;
             session.attach_password(&request.password)?;
             Ok(Shortcut::Done)
         }
